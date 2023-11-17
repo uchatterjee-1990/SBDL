ebe33a6734f26f5d9224ce69d0d1295fdf366a4d
Upal Chatterjee <upal.chatterjee@barclays.com>
2023-11-17 21:21:45 +0000

    first commit

---
+++ hello-world.py
@@ -0,0 +1,20 @@
+from pyspark.sql import *
+import warnings
+
+# Suppress FutureWarnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+if __name__ == "__main__":
+
+    spark = SparkSession.builder \
+                    .master("local[2]") \
+                    .appName("Hello Spark") \
+                    .getOrCreate()
+
+    data_list = [
+        ("Ravi", 20),
+        ("David", 40),
+        ("Abdul", 60)]
+
+    df = spark.createDataFrame(data_list).toDF("Name", "Age")
+    df.show()
